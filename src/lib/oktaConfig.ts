f31e72fdfc31ddd425dc8c857a8a86d78498c39e
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-05 10:52:05 +0000

    LIB-184 added okta configuration

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,9 @@
+export const oktaConfig = {
+
+    clientId: '0oa9u0ejhbnLw6zuz5d7',
+    issuer: 'https://dev-63388380.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
