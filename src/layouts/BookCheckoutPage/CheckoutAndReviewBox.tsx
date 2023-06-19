d7a078f5464b7997a42e951998295ad3afac869e
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-19 09:31:54 +0000

    LIB-217 added review status in Book Checkout component

--- src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BookModel from "../../models/BookModel";
 import { Link } from "react-router-dom";
 
-export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobile: boolean, currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any }> = (props) => {
+export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobile: boolean, currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any, isReviewLeft: boolean }> = (props) => {
 
     function buttonRender() {
         if (props.isAuthenticated){
@@ -20,6 +20,25 @@ export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobil
         }
 
         return (<Link to={'/login'} className='btn btn-success btn-lg'>Sign In</Link>)
+    }
+
+    function reviewRender(){
+        if (props.isAuthenticated && !props.isReviewLeft){
+            return (
+                <p>Leave a review component here.</p>
+            )
+        }else if (props.isAuthenticated && props.isReviewLeft){
+            return (
+                <p><b>Thank you for your review!</b></p>
+            );
+        }
+
+        return (
+            <div>
+                <hr />
+                <p>Sign in to be able to leave a review.</p>
+            </div>
+        )
     }
 
     return (
@@ -58,9 +77,7 @@ export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobil
                 <p className='mt-3'>
                     This number can change until placing order has been complete.
                 </p>
-                <p>
-                    Sign in to be able to leave a review.
-                </p>
+                {reviewRender()}
             </div>
         </div>
     )
