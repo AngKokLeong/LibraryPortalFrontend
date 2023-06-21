da8ae636c63576d8a9a98946277ffaac4944ee0c
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-21 04:00:45 +0000

    LIB-221 created page loading and pagination functions

--- src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
+++ src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ReviewModel from "../../../models/ReviewModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const ReviewListPage = () => {
     
@@ -55,6 +56,27 @@ export const ReviewListPage = () => {
             }  
         )
     }, [currentPage]);
+
+    if (isLoading){
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    if (httpError){
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    const indexOfLastReview: number = currentPage * reviewsPerPage;
+    const indexOfFirstReview: number = indexOfLastReview - reviewsPerPage;
+
+    let lastItem = reviewsPerPage * currentPage <= totalAmountOfReviews ? reviewsPerPage * currentPage : totalAmountOfReviews;
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
     return ();
 }
