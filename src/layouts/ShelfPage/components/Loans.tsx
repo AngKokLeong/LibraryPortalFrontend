671570e11e9481934adf0fceade30dfc3ba4281d
AngKokLeong <53397893+AngKokLeong@users.noreply.github.com>
2023-06-23 10:18:37 +0000

    LIB-232 added useEffect in Loan component

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -1,6 +1,7 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Loans = () => {
     
@@ -14,7 +15,23 @@ export const Loans = () => {
     useEffect(() => {
 
         const fetchUserCurrentLoans = async () => {
+            if (authState && authState.isAuthenticated){
+                const url = `http://localhost:8080/api/books/secure/currentloans`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const shelfCurrentLoansResponse = await fetch(url, requestOptions);
 
+                if (!shelfCurrentLoansResponse.ok){
+                    throw new Error('Something went wrong at fetchUserCurrentLoans()');
+                }
+                const shelfCurrentLoansResponseJson = await shelfCurrentLoansResponse.json();
+            }
+            setIsLoadingUserLoans(false);
         }
         fetchUserCurrentLoans().catch((error: any) => {
             setIsLoadingUserLoans(false);
@@ -24,6 +41,22 @@ export const Loans = () => {
         window.scrollTo(0,0);
 
     }, [authState]);
+
+    if (isLoadingUserLoans){
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    if (httpError){
+        return(
+            <div className='container m-5'>
+                <p>
+                    {httpError}
+                </p>
+            </div>
+        )
+    }
 
     return (
         
