12f07cfe9177bb0fb1c9db24517f5422ba0aed46
Ang Kok Leong <53397893+AngKokLeong@users.noreply.github.com>
2023-09-25 05:13:19 +0000

    LIB-256 added useEffect for History Page

--- src/layouts/ShelfPage/components/HistoryPage.tsx
+++ src/layouts/ShelfPage/components/HistoryPage.tsx
@@ -22,7 +22,21 @@ export const HistoryPage = () => {
         const fetchUserHistory = async () => {
             if (authState && authState.isAuthenticated){
                 const url = `http://localhost:8080/api/histories/search/findBooksByUsserEmail/?userEmail=${authState.accessToken?.claims.sub}&page=${currentPage - 1}&size=5`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const historyResponse = await fetch(url, requestOptions);
+                if (!historyResponse.ok){
+                    throw new Error('Something went wrong!');
+                }
                 
+                const historyResponseJson = await historyResponse.json();
+
+                setHistories(historyResponseJson._embedded.histories);
+                setTotalPages(historyResponseJson.page.totalPages);
             }
         }
         fetchUserHistory().catch((error: any) => {
