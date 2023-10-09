f8ce3f3d179eadbee6f52c991f56a2ddbefa5af1
Ang Kok Leong <53397893+AngKokLeong@users.noreply.github.com>
2023-10-09 16:06:21 +0800

    LIB-289 implement admin messages component (#198)
    * LIB-289 added initial skeleton of AdminMessages component
    
    * LIB-289 added useEffect for AdminMessages component
    
    * LIB-289 added the HTML/CSS for AdminMessages component
    
    * LIB-289 added initial skeleton of AdminMessages component
    
    * LIB-289 added useEffect for AdminMessages component
    
    * LIB-289 added the HTML/CSS for AdminMessages component
    
    ---------
    
    Co-authored-by: Ang Kok Leong <ang.kok.leong.work@gmail.com>

---
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -0,0 +1,92 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { useEffect, useState } from 'react';
+import MessageModel from "../../../models/MessageModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Pagination } from "../../Utils/Pagination";
+
+export const AdminMessages = () => {
+
+    const {authState} = useOktaAuth();
+
+    // Normal Loading Pieces
+    const [isLoadingMessages, setIsLoadingMessages] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    // Messages endpoint state
+    const [messages, setMessages] = useState<MessageModel[]>([]);
+    const [messagesPerPage] = useState(5);
+
+    //Pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchUserMessages = async () => {
+            if (authState && authState?.isAuthenticated) {
+                const url = `http://localhost:8080/api/messages/search/findByClosed?closed=0&page=${currentPage - 1}&size=${messagesPerPage}`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                    
+                };
+                const messagesResponse = await fetch(url, requestOptions);
+                if (!messagesResponse){
+                    throw new Error('Something went wrong!');
+                }
+                const messagesResponseJson = await messagesResponse.json();
+
+                console.log(messagesResponseJson);
+
+                setMessages(messagesResponseJson._embedded.messages);
+                setTotalPages(messagesResponseJson.page.totalPages);
+            }
+            setIsLoadingMessages(false);
+        }
+        fetchUserMessages().catch((error:any) => {
+            setIsLoadingMessages(false);
+            setHttpError(error.message);
+        })
+        window.scrollTo(0, 0);
+
+    }, [authState, currentPage]);
+
+    if (isLoadingMessages){
+        return (
+            <SpinnerLoading />
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+        <div className='mt-3'>
+            {   
+                messages.length > 0 ?
+                <>
+                    <h5>Pending Q/A:</h5>
+                    {   
+                        messages.map(messages => (
+                                <p>Questions that need a response</p>
+
+                            )
+                        )
+                    }
+                </>
+                :
+                <h5>No pending Q/A</h5>
+            }
+            {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} /> }
+        </div>
+    );
+}
