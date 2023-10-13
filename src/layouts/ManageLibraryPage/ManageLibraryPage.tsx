dbf28533b56e0c38a6f6c8e85c33975161f3c0b6
Ang Kok Leong <53397893+AngKokLeong@users.noreply.github.com>
2023-10-13 23:28:55 +0800

    Lib 304 implement add new book component in frontend (#210)
    * LIB-304 added Book states and Displays state in AddNewBook component
    
    * LIB-304 added HTML/CSS for AddNewBook component
    
    * LIB-304 fixed colon at line 5 to semicolon
    
    * LIB-304 added AddNewBook component into ManageLibraryPage
    
    ---------
    
    Co-authored-by: Ang Kok Leong <ang.kok.leong.work@gmail.com>

--- src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import { Redirect } from "react-router-dom";
 import { AdminMessages } from "./components/AdminMessages";
+import { AddNewBook } from "./components/AddNewBook";
 
 
 export const ManageLibraryPage = () => {
@@ -57,7 +58,7 @@ export const ManageLibraryPage = () => {
 
                 <div className='tab-content' id='nav-tabContent'>
                     <div className='tab-pane fade show active' id='nav-add-book' role='tabpanel' aria-labelledby='nav-add-book-tab'>
-                        Add New Book
+                        <AddNewBook />
                     </div>
                     <div className='tab-pane fade' id='nav-quantity' role='tabpanel' aria-labelledby='nav-quantity-tab'>
                         {changeQuantityOfBooksClick ? <>Change Quantity</> : <></>}
