f75d7d3aaf95dbdf30bc6f7d097d2f783caaccb2
Ang Kok Leong <53397893+AngKokLeong@users.noreply.github.com>
2023-10-21 18:11:53 +0800

    LIB-332 modified redirectUri to use HTTPS (#232)
    Co-authored-by: Ang Kok Leong <ang.kok.leong.work@gmail.com>

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -2,7 +2,7 @@ export const oktaConfig = {
 
     clientId: '0oa9u0ejhbnLw6zuz5d7',
     issuer: 'https://dev-63388380.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
